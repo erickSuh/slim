cb08afb326d558ecee385a63a23f7617983ebf40
erickSuh <30279984+erickSuh@users.noreply.github.com>
2018-03-24 08:46:42 -0300

    teste

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
     require 'vendor/autoload.php';
     $app = new \Slim\App();
-    $app->get('/', function($request, $response, $args)
-{
-    echo 'Estou na home';
-});
+    $app->get('/calculadora/{valor1}{operador}{valor2}', function($request, $response, $args)
+    {
+        switch($args['operador'])
+        {
+            case "*":
+            $response -> $args['valor1'] * $args['valor2'];
+            break;
+
+            case "+":
+            $response -> $args['valor1'] + $args['valor2'];
+            break;
+
+            case "/":
+            $response -> $args['valor1'] / $args['valor2'];
+            break;
+
+            case "-":
+            $response -> $args['valor1'] - $args['valor2'];
+            break;
+        }
+        return $response;
+    });
     $app->run();
