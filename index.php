5f14df186b3be3f7056989ab2dd3586711141f2a
Erick Kenji Sugahara <erick.sugahara@gmail.com>
2018-03-30 15:37:41 -0300

    merge

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+    use \Psr\Http\Message\ServerRequestInterface as Request;
+    use \Psr\Http\Message\ResponseInterface as Response;
+
     require 'vendor/autoload.php';
     $app = new \Slim\App();
-<<<<<<< HEAD
-    $app->get('/', function($request, $response, $args)
-    {
-        
-        switch($args['operador'])
-=======
     $app->get('/calculadora/{v1}/{op}/{v2}', function($request, $response, $args)
     {
         $resp = 0;
         switch($args['op'])
->>>>>>> 7476d8745e8a689c91014d834f26311d200e98de
         {
             case "0":
             $resp = $args['v1'] + $args['v2'];
@@ -31,14 +25,8 @@ use \Psr\Http\Message\ResponseInterface as Response;
             $resp = $args['v1'] / $args['v2'];
             break;
         }
-<<<<<<< HEAD
-        $response->getBody()->write("Hello, $response");
-        echo((string)$response);
-        echo("texto");
-=======
         $response = $response->withJson($resp);
         return $response;
->>>>>>> 7476d8745e8a689c91014d834f26311d200e98de
     });
     $app->run();
 ?>
